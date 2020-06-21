7caedfb19ba7949416ee3d3b59622a27877e5b7f
lumat <lumat@op.pl>
2020-06-21 12:25:21 +0200

    change message presentation to <span>

--- src/main/java/pl/gruzini/messenger/controllers/FrontendController.java
+++ src/main/java/pl/gruzini/messenger/controllers/FrontendController.java
@@ -7,19 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.gruzini.messenger.dto.SendMessageDto;
-import pl.gruzini.messenger.model.Message;
 import pl.gruzini.messenger.services.MessageService;
-
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 @Slf4j
 public class FrontendController {
 
     private final MessageService messageService;
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("hh:mm:ss");
 
     public FrontendController(MessageService messageService) {
         this.messageService = messageService;
@@ -29,14 +23,8 @@ public class FrontendController {
     public String index(final Model model) {
         model.addAttribute("time", System.currentTimeMillis());
         model.addAttribute("newMessage", new SendMessageDto());
-        model.addAttribute("allMessages", convertMessagesToText(messageService.readAllPublicMessages()));
+        model.addAttribute("allMessages", messageService.readAllPublicMessages());
         return "index";
-    }
-
-    private List<String> convertMessagesToText(final List<Message> messages) {
-        return messages.stream()
-                .map(message -> message.getTimestamp().format(DATE_TIME_FORMATTER) + " " + message.getUsername() + " : " + message.getText())
-                .collect(Collectors.toList());
     }
 
     @PostMapping
--- src/main/java/pl/gruzini/messenger/model/Message.java
+++ src/main/java/pl/gruzini/messenger/model/Message.java
@@ -3,10 +3,18 @@ package pl.gruzini.messenger.model;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Data
 public class Message {
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("hh:mm:ss");
+
     private final String text;
     private final LocalDateTime timestamp;
     private final String username;
+
+    public String getTimestamp() {
+        return timestamp.format(DATE_TIME_FORMATTER);
+    }
 }
